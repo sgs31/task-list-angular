56353f80537e3ed55fae6e3306d3b3b6092c9fc7
sgs31 <agustinaguirre3195@gmail.com>
2022-02-07 15:30:56 -0300

    se agrego mock de la base de datos

---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,28 @@
+import {Task} from './task'
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras",
+        day: "Agosto 6 a las 9:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Estudiar bootstrap",
+        day: "septiembre 9 a las 13:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Tirar cv",
+        day: "octubre 2 a las 7:00",
+        reminder: true
+    }
+]
