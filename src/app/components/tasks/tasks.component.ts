51094e3e78c1dc5fb91e3092d46f632e15b0e46c
sgs31 <agustinaguirre3195@gmail.com>
2022-02-07 19:12:04 -0300

    nuevos metodos en el servicio task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -22,4 +22,21 @@ export class TasksComponent implements OnInit {
     });
   }
 
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(res =>{
+      let newListTask = this.tasks.filter(item => item.id !== task.id)
+      this.tasks = newListTask
+    })
+  }
+
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task){
+    this.taskService.addTask(task).subscribe(task=>
+      this.tasks.push(task)
+    );
+  }
 }
